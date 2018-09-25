6138a8ff7b40f83d470931ef16c05805f1af1a0c
IvanPryvedenets <40911911+IvanPryvedenets@users.noreply.github.com>
2018-09-25 15:36:44 +0000

    Update The code.py

--- The code.py
+++ The code.py
@@ -256,7 +256,7 @@ for test in (forLoop, listComp, mapCall, genExpr, genFunc):
           (test.__name__,elapsed, result[0], result[-1]))
 
     
-#функція для підрахунку значень іменнованих фргументів
+#функція для підрахунку значень іменнованих aргументів
 def func(**args):
     res = []
     for v in args.values():
@@ -275,6 +275,49 @@ print(adder(a = 3, b = 4))
 
     
 
+#Вдосконалений варіант функції, яка вираховує час виконання коду
+import time, sys
+reps = 1000
+repslist = range(reps)
+
+
+def timer(func, *pargs, **kargs):
+    start = time.clock()
+    for i in repslist:
+        ret = func(*pargs, **kargs)
+    elapsed = time.clock() - start
+    return (elapsed, ret)
+
+
+def forLoop():
+    res = []
+    for x in repslist:
+        res.append(abs(x))
+    return res
+
+def listComp():
+    return [abs(x) for x in repslist]
+
+def mapCall():
+    return list(map(abs, repslist))
+
+def genExpr():
+    return list(abs(x) for x in repslist)
+
+def genFunc():
+    def gen():
+        for x in repslist:
+            yield abs(x)
+    return list(gen())
+
+print(sys.version)
+
+
+for test in (forLoop, listComp, mapCall, genExpr, genFunc):
+    elapsed, result = timer(test)
+    print('-' * 33)
+    print('%-9s: %.5f => [%s...%s]' %
+ (test.__name__, elapsed, result[0], result[-1]))
 
 
 
